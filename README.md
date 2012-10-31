5d72b5fd9186a0a472d1d791be5a05586bc5f118
Erden <erdenoktay@gmail.com>
2012-10-30 18:57:17 -0700

    Defined skeletal structure

--- README.md
+++ README.md
@@ -2,5 +2,53 @@ yearbook
 ========
 
 Creating Write-Up's For Friends
+------------------------------------------
+------------------------------------------
 
+10.30.12 - Started the project environment with Express
+
+
+Yearbook app demonstrates the making of an online yearbook where students can write once for all
+yearbook notes to each other.
+
+
+Packing List
+-----------------
+The application contains the following items:
+
+- README.md -- This file
+- node_modules.[express, jade, stylus]
+- public.[images, javascripts, stylesheets]
+- routes.[index.js, user.js]
+- views.[index.jade, layout.jade]
+- app.js
+- package.json
+
+
+Building
+-----------
+
+
+To Use
+----------
+
+
+To Do List
+--------------
+
+- resolve the issue of localhost not responding
+- add Twitter bootstrap
+- add Facebook login
+
+
+Bugs
+-------
+
+
+Credits and Version History
+-------------------------------------
+
+If you find any problems with this app, please file a bug against it at <erdenoktay89@gmail.com>.
+
+1.0: Initial version.
 by Erden Oktay
--- app.js
+++ app.js
@@ -1,36 +1,78 @@
+var express = require('express');
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
+var app = module.exports = express.createServer();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var articleProvider= new ArticleProvider('localhost', 27017);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+//root
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', { 
+            title: 'Wheaton College 2013 Yearbook Notes',
+            articles:docs
+        });
+    })
 });
+
+//blog
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.jade', {
+        title: 'New Post'
+    });
+});
+
+
+
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.jade',
+        {
+            title: article.title,
+            article:article
+        });
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+//listen
+app.listen(3000);
